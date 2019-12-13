fe54d8ddc9334e966471934bda0b56f1964b268d
calm-block-7153 <rebeccahunt034@gmail.com>
2019-12-13 13:40:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def badge_maker(name)
+  p "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.collect do |name|
+    p "Hello, my name is #{name}."
+  end
+end
+
+def assign_rooms(speaker)
+  speaker.each_with_index.collect do |name,index|
+    p "Hello, #{name}! You'll be assigned to room #{index+1}!"
+  end
+end
+
+def printer(attendees)
+  attendees.each do |print|
+    p "#{batch_badge_creator.first} + #{assign_rooms.first}"
+  end
+end
+  
